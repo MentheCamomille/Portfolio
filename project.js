f699a1d0a6c92c2ccb7e58ff5e493ac1ac5f88ac
Shayman <s.s.1604manganise@email.com>
2023-10-22 22:44:40 +0200

    [MOD]

--- project.js
+++ project.js
@@ -1,60 +1,10 @@
-const videoCardContainer = document.querySelector('.video-container');
+const toggleBtn = document.querySelector('.toggle-btn');
+const sideBar = document.querySelector('.side-bar');
+const videoContainer = document.querySelector('.video-container');
 
-let api_key = "your api key";
-let video_http = "https://www.googleapis.com/youtube/v3/videos?";
-let channel_http = "https://www.googleapis.com/youtube/v3/channels?";
+// Écoutez le clic sur le bouton de bascule
+toggleBtn.addEventListener('click', function () {
+    sideBar.classList.toggle('open'); // Ajoutez ou supprimez la classe "open" pour ouvrir/fermer la sidebar
+    videoContainer.classList.toggle('shifted'); // Décalez le contenu des vidéos pour laisser de la place pour la sidebar
+});
 
-fetch(video_http + new URLSearchParams({
-    key: api_key,
-    part: 'snippet',
-    chart: 'mostPopular',
-    maxResults: 50,
-    regionCode: 'IN'
-}))
-.then(res => res.json())
-.then(data => {
-    data.items.forEach(item => {
-        getChannelIcon(item);
-    })
-})
-.catch(err => console.log(err));
-
-const getChannelIcon = (video_data) => {
-    fetch(channel_http + new URLSearchParams({
-        key: api_key,
-        part: 'snippet',
-        id: video_data.snippet.channelId
-    }))
-    .then(res => res.json())
-    .then(data => {
-        video_data.channelThumbnail = data.items[0].snippet.thumbnails.default.url;
-        makeVideoCard(video_data);
-    })
-}
-
-const makeVideoCard = (data) => {
-    videoCardContainer.innerHTML += `
-    <div class="video" onclick="location.href = 'https://youtube.com/watch?v=${data.id}'">
-        <img src="${data.snippet.thumbnails.high.url}" class="thumbnail" alt="">
-        <div class="content">
-            <img src="${data.channelThumbnail}" class="channel-icon" alt="">
-            <div class="info">
-                <h4 class="title">${data.snippet.title}</h4>
-                <p class="channel-name">${data.snippet.channelTitle}</p>
-            </div>
-        </div>
-    </div>
-    `;
-}
-
-// search bar
-
-const searchInput = document.querySelector('.search-bar');
-const searchBtn = document.querySelector('.search-btn');
-let searchLink = "https://www.youtube.com/results?search_query=";
-
-searchBtn.addEventListener('click', () => {
-    if(searchInput.value.length){
-        location.href = searchLink + searchInput.value;
-    }
-})
