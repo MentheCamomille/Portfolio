4142dadb5f5c7f33f32355f748f020fbcf8636f0
Shayman <s.s.1604manganise@email.com>
2023-10-22 23:37:13 +0200

    add miniatures and skills

--- project.js
+++ project.js
@@ -1,6 +1,6 @@
 const toggleBtn = document.querySelector('.toggle-btn');
 const sideBar = document.querySelector('.side-bar');
-const videoContainer = document.querySelector('.video-container');
+const videoContainer = document.getElementById("video-container");
 
 // Écoutez le clic sur le bouton de bascule
 toggleBtn.addEventListener('click', function () {
@@ -8,3 +8,96 @@ toggleBtn.addEventListener('click', function () {
     videoContainer.classList.toggle('shifted'); // Décalez le contenu des vidéos pour laisser de la place pour la sidebar
 });
 
+const projects = [
+    {
+        title: "Chat",
+        imageUrl: "chat.png",
+        description: "Description du projet 1",
+        // Autres informations du projet
+    },
+    {
+        title: "Random Color",
+        imageUrl: "color.png",
+        description: "Description du projet 2",
+        // Autres informations du projet
+    },
+    {
+        title: "Hyrule Castle",
+        imageUrl: "image2.jpg",
+        description: "Description du projet 2",
+        // Autres informations du projet
+    },
+    {
+        title: "Aminy",
+        imageUrl: "render3.png",
+        description: "Description du projet 2",
+        // Autres informations du projet
+    },
+    {
+        title: "Tic Tac Toe",
+        imageUrl: "tic.png",
+        description: "Description du projet 2",
+        // Autres informations du projet
+    },
+    {
+        title: "E-commerce website",
+        imageUrl: "image2.jpg",
+        description: "Description du projet 2",
+        // Autres informations du projet
+    },
+    {
+        title: "Space Attack",
+        imageUrl: "space.png",
+        description: "Description du projet 2",
+        // Autres informations du projet
+    },
+    {
+        title: "Dashboard",
+        imageUrl: "Dashboard.png",
+        description: "Description du projet 2",
+        // Autres informations du projet
+    },
+    {
+        title: "3D plan hospital",
+        imageUrl: "gnn2.jpg",
+        description: "Description du projet 2",
+        // Autres informations du projet
+    },
+    {
+        title: "Maquettes Figma",
+        imageUrl: "image2.jpg",
+        description: "Description du projet 2",
+        // Autres informations du projet
+    },
+    {
+        title: "Roc, Paper, Scisors",
+        imageUrl: "image2.jpg",
+        description: "Description du projet 2",
+        // Autres informations du projet
+    }
+    // Ajoutez d'autres projets
+];
+
+// Fonction pour afficher les miniatures d'images de projets
+function displayProjectThumbnails(projects) {
+    videoContainer.innerHTML = "";
+
+    projects.forEach((project) => {
+        const projectTitle = project.title;
+        const projectImageUrl = project.imageUrl;
+        const projectDescription = project.description;
+
+        const projectThumbnail = document.createElement("div");
+        projectThumbnail.className = "project-thumbnail";
+        projectThumbnail.innerHTML = `
+            <img src="${projectImageUrl}" alt="${projectTitle}">
+            <h3>${projectTitle}</h3>
+            <p>${projectDescription}</p>
+        `;
+
+        videoContainer.appendChild(projectThumbnail);
+    });
+}
+
+// Appel de la fonction pour afficher les miniatures d'images de projets
+displayProjectThumbnails(projects);
