2d34ea4cab6a9bf891cff672db0ae9ee97f302cf
Shayman <s.s.1604manganise@email.com>
2023-10-23 15:52:30 +0200

    sidebar

--- js/project.js
+++ js/project.js
@@ -1,12 +1,25 @@
 const toggleBtn = document.querySelector('.toggle-btn');
 const sideBar = document.querySelector('.side-bar');
+const filters = document.querySelector('.filters');
 const videoContainer = document.getElementById("video-container");
 
-// Écoutez le clic sur le bouton de bascule
 toggleBtn.addEventListener('click', function () {
-    sideBar.classList.toggle('open'); // Ajoutez ou supprimez la classe "open" pour ouvrir/fermer la sidebar
-    videoContainer.classList.toggle('shifted'); // Décalez le contenu des vidéos pour laisser de la place pour la sidebar
+    sideBar.classList.toggle('open');
+    filters.classList.toggle('open');
+    videoContainer.classList.toggle('open');
+
+    // Ajouter ou supprimer les styles en fonction de l'état de la barre latérale
+    if (sideBar.classList.contains('open')) {
+        // Si la barre latérale est ouverte, rétablir les styles par défaut
+        filters.style.flex = "0 0 250px";
+        videoContainer.style.marginLeft = "250px";
+    } else {
+        // Si la barre latérale est fermée, ajuster les styles pour prendre l'espace disponible
+        filters.style.flex = "1"; // Les filtres occupent tout l'espace disponible
+        videoContainer.style.marginLeft = "120px"; // Aucun décalage à gauche
+    }
 });
+
 
 const projects = [
     {
